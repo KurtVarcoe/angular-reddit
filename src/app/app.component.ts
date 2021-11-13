4cb41a2b9cba887a9ee3dfb731b5436ed61d4323
Kurt <kurtvarcoe@gmail.com>
2021-11-13 23:00:26 +0200

    addArticle method now adds article based on the input values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   addArticle(title: HTMLInputElement, link:HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0);
+    title.value = '';
+    link.value = '';
     return false;
   }
 }
